9774f3e585f8083cae11e7439388bde67ef74d2f
Karm <karmtawfiq@gmail.com>
2026-08-09 19:33:07 +0100

    Created entry-point method for new limit orders

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,42 @@ struct Order_location
 {
 	bool is_buy{};
 	float limit_price{};
-	std::list<unsigned int>::iterator c_it{};
+	std::list<Order>::iterator c_it{};
 };
 
 std::map<float, std::list<Order>, std::greater<float>> bids;
 std::map<float, std::list<Order>> asks;
 std::unordered_map<unsigned int, Order_location> index_map;
+
+
+void accept_order(unsigned int id, bool is_buy, unsigned short quantity, bool is_limit, float limit_price, std::uint64_t timestamp)
+{
+	Order new_order { id, is_buy, quantity, is_limit, limit_price, timestamp };
+
+	if (is_limit)
+	{
+		if (is_buy)
+		{
+			bids[limit_price].push_back(new_order);
+			auto it { std::prev(bids[limit_price].end())};
+
+			index_map[id] = { 1, limit_price, it };
+		}
+		else
+		{
+			asks[limit_price].push_back(new_order);
+			auto it{ std::prev(asks[limit_price].end()) };
+
+			index_map[id] = { 0, limit_price, it };
+
+		}
+	}
+	else
+	{
+		// Processing for market orders goes here...
+	}
+
+}
 
 int main()
 {
